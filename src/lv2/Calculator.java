77faad566ad0edbd935fa6958868812339a01d48
dain391 <0901ekdls@gmail.com>
2025-04-17 15:33:47 +0900

    Level 1,2

---
+++ src/lv2/Calculator.java
@@ -0,0 +1,51 @@
+package lv2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calculator {
+    private List<Double> results;
+
+    public Calculator() {
+        results = new ArrayList<>();
+    }
+
+    public double calculate(double n1, double n2, char op) {
+        double result = 0;
+        switch (op) {
+            case '+', '➕' -> result = n1 + n2;
+            case '-', '➖' -> result = n1 - n2;
+            case '*', '✖' -> result = n1 * n2;
+            case '/', '➗' -> {
+                if (n2 == 0) {
+                    System.out.println("0으로 나눌 수 없습니다.\n");
+                    return -1;
+                } else {
+                    result = n1 / n2;
+                }
+            }
+            default -> {
+                return -2;
+            }
+        }
+        results.add(result);
+        return result;
+    }
+
+    public void removeR() {
+        if (!results.isEmpty()) {
+            results.remove(0);
+            System.out.println("첫 번째 계산 결과가 삭제되었습니다.");
+        } else {
+            System.out.println("삭제할 데이터가 없습니다.");
+        }
+    }
+
+    public List<Double> getR() {
+        return results;
+    }
+
+    public void setR(List<Double> results) {
+        this.results = results;
+    }
+}
