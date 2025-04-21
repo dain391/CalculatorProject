7168f39cf7ebf6a70232c64150a17f1736534e91
dain391 <0901ekdls@gmail.com>
2025-04-21 20:04:53 +0900

    Level 2

---
+++ src/App.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Calculator calculator = new Calculator();
+
+        while (true) {
+            double n1 = 0;
+            while (true) {
+                System.out.print("\n첫 번째 숫자를 입력하세요: ");
+                String input = sc.next();
+                try {
+                    n1 = Double.parseDouble(input);
+                    break;
+                } catch (NumberFormatException e) {
+                    System.out.println("잘못된 입력입니다. 숫자를 입력해주세요.");
+                }
+            }
+
+            char op = ' ';
+            while (true) {
+                System.out.print("연산자(➕, ➖, ✖️, ➗)를 입력하세요: ");
+                String operator = sc.next();
+                op = operator.charAt(0);
+                if (op == '+' || op == '-' || op == '*' || op == '/' || op == '➕' || op == '➖' || op == '✖' || op == '➗') {
+                    break;
+                } else {
+                    System.out.println("잘못된 연산자입니다. 다시 입력해주세요.\n");
+                }
+            }
+
+            double n2 = 0;
+            while (true) {
+                System.out.print("두 번째 숫자를 입력하세요: ");
+                String input = sc.next();
+                try {
+                    n2 = Double.parseDouble(input);
+                    break;
+                } catch (NumberFormatException e) {
+                    System.out.println("잘못된 입력입니다. 숫자를 입력해주세요.\n");
+                }
+            }
+
+            double result = calculator.calculate(n1, n2, op);
+
+            if (result == -1) {
+                System.out.println("0으로 나눌 수 없습니다.");
+                continue;
+            } else if (result != -2) {
+                if (result == (int) result) {
+                    System.out.println("\n계산 결과: " + (int) result);
+                } else {
+                    System.out.println("\n계산 결과: " + result);
+                }
+
+                System.out.print("계산 결과를 삭제하시겠습니까? ('yes' 입력 시 삭제): ");
+                String del = sc.next();
+                if (del.equals("yes")) {
+                    calculator.removeR();
+                }
+            }
+
+            System.out.print("계속하시겠습니까?('exit' 입력 시 종료): ");
+            String ans = sc.next();
+            if (ans.equals("exit")) {
+                System.out.println("계산기를 종료합니다.");
+                break;
+            }
+        }
+        sc.close();
+    }
+}
--- src/Calculator.java
+++ src/Calculator.java
@@ -1,79 +1,49 @@
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Calculator {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    private List<Double> results;
 
-        while (true) {
-            double n1 = 0;
-            while (true) {
-                System.out.print("\n첫 번째 숫자를 입력하세요: ");
-                String input = scanner.nextLine();
-                try {
-                    n1 = Double.parseDouble(input);
-                    break;
-                } catch (NumberFormatException e) {
-                    System.out.println("잘못된 입력입니다. 숫자를 입력해주세요.");
-                }
-            }
+    public Calculator() {
+        results = new ArrayList<>();
+    }
 
-            char op;
-            while (true) {
-                System.out.print("연산자(➕, ➖, ✖️, ➗)를 입력하세요: ");
-                String operator = scanner.nextLine();
-                if (operator.length() == 0) continue;
-                op = operator.charAt(0);
-
-                if (op == '+' || op == '-' || op == '*' || op == '/' || op == '➕' || op == '➖' || op == '✖' || op == '➗') {
-                    break;
+    public double calculate(double n1, double n2, char op) {
+        double result = 0;
+        switch (op) {
+            case '+', '➕' -> result = n1 + n2;
+            case '-', '➖' -> result = n1 - n2;
+            case '*', '✖' -> result = n1 * n2;
+            case '/', '➗' -> {
+                if (n2 == 0) {
+                    System.out.println("0으로 나눌 수 없습니다.\n");
+                    return -1;
                 } else {
-                    System.out.println("잘못된 연산자입니다. 다시 입력해주세요.\n");
+                    result = n1 / n2;
                 }
             }
-
-            double n2 = 0;
-            while (true) {
-                System.out.print("두 번째 숫자를 입력하세요: ");
-                String input = scanner.nextLine();
-                try {
-                    n2 = Double.parseDouble(input);
-                    break;
-                } catch (NumberFormatException e) {
-                    System.out.println("잘못된 입력입니다. 숫자를 입력해 주세요.\n");
-                }
-            }
-
-            double result = 0;
-            boolean valid = true;
-
-            switch (op) {
-                case '+', '➕' -> result = n1 + n2;
-                case '-', '➖' -> result = n1 - n2;
-                case '*', '✖' -> result = n1 * n2;
-                case '/', '➗' -> {
-                    if (n2 == 0) {
-                        System.out.println("0으로 나눌 수 없습니다.\n");
-                        valid = false;
-                    } else {
-                        result = n1 / n2;
-                    }
-                }
-            }
-
-            if (valid) {
-                if (result == (int) result) {
-                    System.out.println("\n계산 결과: " + (int) result);
-                } else {
-                    System.out.println("\n계산 결과: " + result);
-                }
-            }
-
-            System.out.print("계속하시겠습니까?('exit' 입력 시 종료): ");
-            String ans = scanner.nextLine();
-            if (ans.equals("exit")) {
-                System.out.println("계산기를 종료합니다.");
-                break;
+            default -> {
+                return -2;
             }
         }
+        results.add(result);
+        return result;
+    }
+
+    public void removeR() {
+        if (!results.isEmpty()) {
+            results.remove(0);
+            System.out.println("첫 번째 계산 결과가 삭제되었습니다.\n");
+        } else {
+            System.out.println("삭제할 데이터가 없습니다.");
+        }
+    }
+
+    public List<Double> getR() {
+        return results;
+    }
+
+    public void setR(List<Double> results) {
+        this.results = results;
     }
 }
